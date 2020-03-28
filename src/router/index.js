434798099f444618547e8b788c04bc72d996f039
Elie Andraos <elie@andraos.org>
2020-03-28 17:55:46 +0200

    :zap: Dynamically import route files

--- src/router/index.js
+++ src/router/index.js
@@ -3,18 +3,33 @@ import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
 
-const routes = [
+let routes = [
     {
         path: '/',
         component: () => {
-            return import("./../views/home/Index");
+            return import("@/views/home/Index");
         },
         name: 'home'
     },
 ];
 
+/**
+ * Dynamically load custom route files
+ * so this file does not grow big.
+ */
+let files = require.context('./', true, /\.js$/i);
+
+files.keys().map((key) => {
+    let filename = (key.split('/').pop().split('.')[0]);
+    if(filename !== 'index') {
+        let fileRoutes = require('./' + filename + '.js');
+        routes = routes.concat(fileRoutes.default);
+    }
+});
+
 const router = new VueRouter({
-    routes: routes
+    routes: routes,
+    mode: history
 });
 
 export default router;
