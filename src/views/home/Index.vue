a7d0b50c6c8de9d38f53a4c4c700580a5908be81
Elie Andraos <elie@andraos.org>
2020-03-29 18:58:47 +0300

    :lipstick: Setup homepage

--- src/views/home/Index.vue
+++ src/views/home/Index.vue
@@ -1,3 +1,12 @@
 <template>
-    <div class="w-full h-full flex items-center justify-center">Project Rainbow</div>
+    <div class="w-full h-full flex items-center justify-center bg-gray-100">
+        <div class="text-center">
+            <img src="@/assets/logo.svg" class="inline w-64" />
+            <div class="text-gray-600 text-xl font-bold">Project Rainbow</div>
+            <div>SPA starter kit with couple of UI components built with Vue & Tailwind</div>
+            <router-link :to="{name: 'docs'}" class="text-white font-medium mt-8 inline-block bg-orange-300 w-48 rounded-md p-4">
+                Visit the docs
+            </router-link>
+        </div>
+    </div>
 </template>
