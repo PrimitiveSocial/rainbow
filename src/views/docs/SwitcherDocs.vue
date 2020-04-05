cb7de628a8c637594986ec4813f8f8a734561f35
Elie Andraos <elie@andraos.org>
2020-04-05 18:54:46 +0300

    :pencil2: Fix title page in Switcher docs

--- src/views/docs/SwitcherDocs.vue
+++ src/views/docs/SwitcherDocs.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div class="max-w-7xl mx-auto px-4 pb-4 sm:px-6 lg:px-8 flex justify-between border-b mb-8">
-            <h1 class="text-2xl font-semibold text-gray-900">Toggle Buttons</h1>
+            <h1 class="text-2xl font-semibold text-gray-900">Switcher</h1>
             <features :options="features"></features>
         </div>
 
