5edc2a1970e3ed2a8684d937acbc9024f3ce5153
Elie Andraos <elie@andraos.org>
2020-04-11 19:50:15 +0300

    :boom: Add uploader component

---
+++ src/components/rainbow/uploader/ImageEditor.vue
@@ -0,0 +1,110 @@
+<template>
+    <div class="flex w-full justify-center">
+        <div class="h-auto w-3/5">
+            <vue-cropper
+                ref="cropper"
+                :src="image"
+                :cropBoxResizable="false"
+                :viewMode="2"
+                :zoomOnWheel="false"
+                :aspectRatio="aspectRatio"
+                :dragMode="'move'"
+            >
+            </vue-cropper>
+        </div>
+        <div class="ml-4">
+            <button class="btn-primary text-xs mb-4 block" @click="crop()">
+                <svg class="h-4 w-4 inline" viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                        <g class="fill-current">
+                            <path d="M14,16 L14,20 L16,20 L16,16 L20,16 L20,14 L6,14 L6,0 L4,0 L4,4 L0,4 L0,6 L4,6 L4,14.0059397 C4,15.1054862 4.8927712,16 5.99406028,16 L14,16 Z M14,13 L14,6 L7,6 L7,4 L14.0059397,4 C15.1072288,4 16,4.89451376 16,5.99406028 L16,13 L14,13 Z" id="Combined-Shape"></path>
+                        </g>
+                    </g>
+                </svg>
+            </button>
+            <button class="btn-primary text-xs mb-4 block" @click="zoom(0.2)">
+                <svg class="h-4 w-4 inline" viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                        <g class="fill-current">
+                            <path d="M7,7 L5,7 L5,9 L7,9 L7,11 L9,11 L9,9 L11,9 L11,7 L9,7 L9,5 L7,5 L7,7 Z M12.9056439,14.3198574 C11.5509601,15.3729184 9.84871145,16 8,16 C3.581722,16 0,12.418278 0,8 C0,3.581722 3.581722,0 8,0 C12.418278,0 16,3.581722 16,8 C16,9.84871145 15.3729184,11.5509601 14.3198574,12.9056439 L19.6568542,18.2426407 L18.2426407,19.6568542 L12.9056439,14.3198574 Z M8,14 C11.3137085,14 14,11.3137085 14,8 C14,4.6862915 11.3137085,2 8,2 C4.6862915,2 2,4.6862915 2,8 C2,11.3137085 4.6862915,14 8,14 Z" id="Combined-Shape"></path>
+                        </g>
+                    </g>
+                </svg>
+            </button>
+            <button class="btn-primary text-xs mb-4 block" @click="zoom(-0.2)">
+                <svg class="h-4 w-4 inline" viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                        <g class="fill-current">
+                            <path d="M12.9056439,14.3198574 C11.5509601,15.3729184 9.84871145,16 8,16 C3.581722,16 0,12.418278 0,8 C0,3.581722 3.581722,0 8,0 C12.418278,0 16,3.581722 16,8 C16,9.84871145 15.3729184,11.5509601 14.3198574,12.9056439 L19.6568542,18.2426407 L18.2426407,19.6568542 L12.9056439,14.3198574 Z M8,14 C11.3137085,14 14,11.3137085 14,8 C14,4.6862915 11.3137085,2 8,2 C4.6862915,2 2,4.6862915 2,8 C2,11.3137085 4.6862915,14 8,14 Z M5,7 L11,7 L11,9 L5,9 L5,7 Z" id="Combined-Shape"></path>
+                        </g>
+                    </g>
+                </svg>
+            </button>
+            <button class="btn-primary text-xs mb-4 block" @click="rotate(45)">
+                <svg class="h-4 w-4 inline" viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                        <g class="fill-current">
+                            <path d="M14.6568542,15.6568542 C13.209139,17.1045695 11.209139,18 9,18 C4.581722,18 1,14.418278 1,10 C1,5.581722 4.581722,2 9,2 C13.418278,2 17,5.581722 17,10 L15,10 C15,6.6862915 12.3137085,4 9,4 C5.6862915,4 3,6.6862915 3,10 C3,13.3137085 5.6862915,16 9,16 C10.6568542,16 12.1568542,15.3284271 13.2426407,14.2426407 L14.6568542,15.6568542 L14.6568542,15.6568542 Z M12,10 L20,10 L16,14 L12,10 L12,10 Z" id="Combined-Shape"></path>
+                        </g>
+                    </g>
+                </svg>
+            </button>
+            <button class="btn-primary text-xs mb-4 block" @click="reset()">
+                <svg class="h-4 w-4 inline" viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                        <g class="fill-current">
+                            <path d="M9.42239525,7.09396812 C10.2387498,5.25806681 9.89409813,3.03105216 8.38844025,1.52539428 C6.93864576,0.0755997864 4.82002948,-0.297742297 3.02577331,0.40536803 L6.97422669,4.35382141 L4.14579956,7.18224853 L0.197346187,3.23379515 C-0.50576414,5.02805132 -0.132422056,7.1466676 1.31737244,8.59646209 C2.70997507,9.98906472 4.71967257,10.3884681 6.46624524,9.79467236 L16.5961941,19.9246212 L19.4246212,17.0961941 L9.42239525,7.09396812 Z" id="Combined-Shape"></path>
+                        </g>
+                    </g>
+                </svg>
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+    import VueCropper from 'vue-cropperjs';
+    import 'cropperjs/dist/cropper.css';
+
+    export default {
+        components: { VueCropper },
+        props: {
+            image: {
+                type: String,
+                required: true
+            },
+            index: {
+                type: Number,
+                required: true
+            },
+            aspectRatio: {
+                type: Number,
+                required: true,
+                default: 1
+            }
+        },
+        methods: {
+            crop() {
+                let base64Image = this.$refs.cropper.getCroppedCanvas().toDataURL();
+                this.$refs.cropper.replace(base64Image);
+                this.$emit('image-cropped', {index: this.index, image: base64Image});
+            },
+            reset() {
+                this.$refs.cropper.reset();
+            },
+            zoom(percent) {
+                this.$refs.cropper.relativeZoom(percent);
+            },
+            rotate(deg) {
+                this.$refs.cropper.rotate(deg);
+            },
+            cropOnEnterKey(event) {
+                if(event.keyCode === 13)
+                    this.crop();
+            }
+        },
+        mounted() {
+            document.addEventListener("keyup", this.cropOnEnterKey);
+        }
+    }
+</script>
---
+++ src/components/rainbow/uploader/Uploader.vue
@@ -0,0 +1,194 @@
+<template>
+    <div>
+        <slot name="default">
+            <button @click="$modal.open('uploader')" class="btn-primary">Upload</button>
+        </slot>
+
+        <modal name="uploader" size="large">
+            <input type="file" ref="upload-input" class="hidden" :multiple="isMultipleUpload" accept="image/*" @change="previewImages">
+
+            <div v-if="hasUploadedImages" class="bg-gray-100 p-4 mb-16">
+                <div class="w-full m-auto flex justify-between items-center">
+                    <div>
+                        <button class="btn-primary mr-4" @click="openFileDialog" v-if="isMultipleUpload">
+                            <svg class="h-4 w-4 inline" viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                                <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                    <g class="fill-current">
+                                        <path d="M11,9 L11,5 L9,5 L9,9 L5,9 L5,11 L9,11 L9,15 L11,15 L11,11 L15,11 L15,9 L11,9 Z M10,20 C15.5228475,20 20,15.5228475 20,10 C20,4.4771525 15.5228475,0 10,0 C4.4771525,0 0,4.4771525 0,10 C0,15.5228475 4.4771525,20 10,20 Z M10,18 C14.418278,18 18,14.418278 18,10 C18,5.581722 14.418278,2 10,2 C5.581722,2 2,5.581722 2,10 C2,14.418278 5.581722,18 10,18 Z" id="Shape"></path>
+                                    </g>
+                                </g>
+                            </svg>
+                            Add more
+                        </button>
+                        <button class="btn-success" @click="upload">
+                            <svg class="h-4 w-4 inline" viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" >
+                                <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                    <g class="fill-current">
+                                        <path d="M16.8792928,9.09695343 C18.6654343,9.4976045 20,11.09295 20,13 C20,15.209139 18.209139,17 16,17 L5,17 C2.23857625,17 0,14.7614237 0,12 C0,9.58046798 1.71857515,7.56233069 4.00162508,7.09968852 C4.00054449,7.06659179 4,7.03335948 4,7 C4,5.34314575 5.34314575,4 7,4 C7.55384606,4 8.07263826,4.1500834 8.51792503,4.41179863 C9.4182103,3.53797709 10.6462795,3 12,3 C14.7614237,3 17,5.23857625 17,8 C17,8.37684164 16.9583108,8.74394625 16.8792928,9.09695343 Z M10,7 L14,11 L6,11 L10,7 Z M9,11 L11,11 L11,14 L9,14 L9,11 Z" id="Combined-Shape"></path>
+                                    </g>
+                                </g>
+                            </svg>
+                            Upload
+                        </button>
+                    </div>
+                    <div class="font-bold mr-16" v-if="isMultipleUpload">
+                        Showing image {{ imageCounter }}
+                        <svg
+                            class="h-4 w-4 inline cursor-pointer mx-2"
+                            viewBox="0 0 20 20"
+                            version="1.1"
+                            xmlns="http://www.w3.org/2000/svg"
+                            xmlns:xlink="http://www.w3.org/1999/xlink"
+                            @click="currentImageIndex--"
+                            v-if="canGoBackward"
+                        >
+                            <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                <g class="fill-current">
+                                    <polygon id="Combined-Shape" points="10 13 18 13 18 7 10 7 10 2 2 10 10 18 10 13"></polygon>
+                                </g>
+                            </g>
+                        </svg>
+                        <svg
+                            class="h-4 w-4 inline cursor-pointer mx-2"
+                            viewBox="0 0 20 20"
+                            version="1.1"
+                            xmlns="http://www.w3.org/2000/svg"
+                            xmlns:xlink="http://www.w3.org/1999/xlink"
+                            @click="currentImageIndex++"
+                            v-if="canGoForward"
+                        >
+                            <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                <g class="fill-current">
+                                    <polygon id="Combined-Shape" points="10 7 2 7 2 13 10 13 10 18 18 10 10 2 10 7"></polygon>
+                                </g>
+                            </g>
+                        </svg>
+                    </div>
+                </div>
+            </div>
+
+            <div v-if="hasUploadedImages" class="mb-8">
+                <div v-for="(image, index) in uploadedImages" :key="index">
+                    <div v-if="index === currentImageIndex">
+                        <image-editor
+                            :image="image.base64"
+                            :index="index"
+                            :aspect-ratio="aspectRatio"
+                            @image-cropped="cropImage"
+                        ></image-editor>
+                    </div>
+                </div>
+            </div>
+
+            <div v-else class="text-indigo-300 w-full h-full flex justify-center items-center">
+                <div class="text-center">
+                    <svg class="h-48 w-48 block mb-4" viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                        <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                            <g class="fill-current">
+                                <path d="M11,13 L8,10 L2,16 L11,16 L18,16 L13,11 L11,13 Z M0,3.99406028 C0,2.8927712 0.898212381,2 1.99079514,2 L18.0092049,2 C19.1086907,2 20,2.89451376 20,3.99406028 L20,16.0059397 C20,17.1072288 19.1017876,18 18.0092049,18 L1.99079514,18 C0.891309342,18 0,17.1054862 0,16.0059397 L0,3.99406028 Z M15,9 C16.1045695,9 17,8.1045695 17,7 C17,5.8954305 16.1045695,5 15,5 C13.8954305,5 13,5.8954305 13,7 C13,8.1045695 13.8954305,9 15,9 Z" id="Combined-Shape"></path>
+                            </g>
+                        </g>
+                    </svg>
+
+                    <button class="btn-primary" @click="openFileDialog">
+                        <svg class="h-4 w-4 inline mr-2" viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" @click="$emit('click')">
+                            <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                <g class="fill-current">
+                                    <path d="M11,9 L11,5 L9,5 L9,9 L5,9 L5,11 L9,11 L9,15 L11,15 L11,11 L15,11 L15,9 L11,9 Z M10,20 C15.5228475,20 20,15.5228475 20,10 C20,4.4771525 15.5228475,0 10,0 C4.4771525,0 0,4.4771525 0,10 C0,15.5228475 4.4771525,20 10,20 Z M10,18 C14.418278,18 18,14.418278 18,10 C18,5.581722 14.418278,2 10,2 C5.581722,2 2,5.581722 2,10 C2,14.418278 5.581722,18 10,18 Z" id="Shape"></path>
+                                </g>
+                            </g>
+                        </svg>
+                        Add photo
+                    </button>
+                </div>
+            </div>
+        </modal>
+
+    </div>
+</template>
+
+<script>
+    import Modal from "../Modal";
+    import ImageEditor from "./ImageEditor";
+
+    export default {
+        components: { Modal, ImageEditor},
+        props: {
+            aspectRatio: {
+                type: Number,
+                required: true,
+                default: 1
+            },
+            maxImages: {
+                type: Number,
+                required: false,
+                default: 5
+            }
+        },
+        data: () => {
+            return {
+                uploadedImages: [],
+                currentImageIndex: 0
+            }
+        },
+        computed: {
+            hasUploadedImages() {
+                return this.uploadedImages.length;
+            },
+            isMultipleUpload() {
+                return this.maxImages > 1;
+            },
+            imageCounter() {
+                return (this.currentImageIndex + 1) + ' of ' + this.uploadedImages.length;
+            },
+            canGoBackward() {
+                return (this.hasUploadedImages && this.currentImageIndex > 0);
+            },
+            canGoForward() {
+                return (this.hasUploadedImages && this.currentImageIndex < (this.uploadedImages.length-1));
+            }
+        },
+        methods: {
+            openFileDialog() {
+                this.$refs['upload-input'].click();
+            },
+            previewImages() {
+                let _files = this.$refs['upload-input'].files;
+
+                _files.forEach((file) => {
+                    let reader = new FileReader();
+                    reader.onload = (event) => {
+                        this.uploadedImages.push( {
+                            base64: event.target.result,
+                            name: file.name,
+                            size: file.size
+                        });
+                    };
+                    reader.readAsDataURL(file);
+                });
+            },
+            cropImage(payload) {
+                this.uploadedImages[payload.index]['base64'] = payload.image;
+            },
+            upload() {
+                this.$emit('photos-uploaded', { photos: this.uploadedImages});
+                this.uploadedImages = [];
+                this.currentImageIndex = 0;
+                this.$modal.close('uploader');
+            },
+            navigate(event) {
+                if(event.keyCode === 37 && this.canGoBackward) {
+                    this.currentImageIndex--;
+                }
+
+                if(event.keyCode === 39 && this.canGoForward) {
+                    this.currentImageIndex++;
+                }
+            }
+        },
+        mounted() {
+            // Add arrow left/right navigation support
+            document.addEventListener("keyup", this.navigate);
+        }
+    }
+</script>
