262bcecbd56dedf1011824b07cfe90a69375c7b0
Elie Andraos <elie@andraos.org>
2020-05-19 15:41:39 +0300

    :bulb: validation docs and examples

--- src/router/docs.js
+++ src/router/docs.js
@@ -4,6 +4,11 @@ const routes = [
         path: '/docs',
         component: () => { return import("@/views/docs/Index") },
     },
+    {
+        name: 'validation',
+        path: '/validation',
+        component: () => { return import("@/views/docs/Validation") },
+    },
 ];
 
 export default routes;
---
+++ src/views/docs/Validation.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="mt-8 mx-auto w-1/3">
+        <p>
+            <input type="text" class="border p-2 mb-4 mr-2 focus:outline-none" placeholder="email" />
+            <span class="text-red-700 text-sm"></span>
+        </p>
+
+        <p>
+            <input type="text" class="border p-2 mb-4 mr-2 focus:outline-none" placeholder="username" />
+            <span class="text-red-700 text-sm"></span>
+        </p>
+
+        <p>
+            <input type="text" class="border p-2 mb-4 mr-2 focus:outline-none" placeholder="domain" />
+            <span class="text-red-700 text-sm"></span>
+        </p>
+
+        <p>
+            <input type="text" class="border p-2 mb-4 mr-2 focus:outline-none" placeholder="registration date" />
+            <span class="text-red-700 text-sm"></span>
+        </p>
+    </div>
+
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
